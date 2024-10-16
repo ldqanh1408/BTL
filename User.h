ef6209cceaeb499ed4bfcd12b024057f46b3cbc2
Dka PTIT <ekko.okke666@gmail.com>
2024-10-16 15:44:10 +0700

    second commit

--- User.h
+++ User.h
@@ -4,17 +4,31 @@ using namespace std;
 class User {
 private:
     string full_name, address, country, phone_number;
-    int age;
-    bool gender;
-    //thêm 1 số thuộc tính
+    int age; bool gender;
+
+    string account, password;
 public:
-    User(const string& _full_name, const string& _address, const string& _country, const string& _phone_number, const int& _age, const bool& _gender) {
-        this->full_name = _full_name;
-        this->address = _address;
-        this->country = _country;
-        this->phone_number = _phone_number;
-        this->age = _age;
-        this->gender = _gender;
+    User() {}
+
+    friend istream& operator>> (istream &in, User &who) {
+        cout << "Nhap thong tin nguoi dung:" << endl;
+        cout << "Ho va ten: ";
+        getline(in, who.full_name);
+        cout << endl << "Ten dang nhap: ";
+        getline(in, who.account);
+        cout << endl << "Tuoi: ";
+        in >> who.age;
+        cout << endl << "Dia chi: ";
+        getline(in, who.address);
+        cout << endl << "Quoc gia: ";
+        getline(in, who.country);
+        cout << endl << "So dien thoai: ";
+        getline(in, who.phone_number);
+        cout << endl << "Gioi tinh (1: Nam, 0: Nu): ";
+        in >> who.gender;
+        cout << endl << "Mat khau: ";
+        getline(in, who.password);
+
+        return in;
     }
-    //thêm code
 };
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
+#include"User.h"
 using namespace std;
 
 int main() {
+    User A; cin >> A;
     return 0;
 }
