2de680a24b9f7e687d48d1485179ec91b90d8fb0
Din <n23dccn146@student.ptithcm.edu.vn>
2024-10-31 14:10:18 +0700

    2 commit

--- main.cpp
+++ main.cpp
@@ -6,56 +6,58 @@
 #include"gotp.h"
 #include"bcrypt.cpp"
 #include"blowfish.cpp"
+#include"Console.h"
 
 using namespace std;
 namespace fs = filesystem;
 
 
 
-bool create_account() {
-    auto generate_password = [&](User &person)->string {
-        string first_name = "", full_name = person.get_full_name();
-        for(int i = full_name.size() - 1; i >= 0 && full_name[i] != ' '; --i) {
-            first_name = full_name[i] + first_name;
-        }
+// bool create_account() {
+//     auto generate_password = [&](User &person)->string {
+//         string first_name = "", full_name = person.get_full_name();
+//         for(int i = full_name.size() - 1; i >= 0 && full_name[i] != ' '; --i) {
+//             first_name = full_name[i] + first_name;
+//         }
 
-        return first_name + "&" + to_string(person.get_age()) + "&" + to_string(int(person.get_gender()));
-    };
+//         return first_name + "&" + to_string(person.get_age()) + "&" + to_string(int(person.get_gender()));
+//     };
 
-    cout << "Tên tài khoản: ";
-    string new_account; cin >> new_account;
-    cout << endl;
+//     cout << "Tên tài khoản: ";
+//     string new_account; cin >> new_account;
+//     cout << endl;
 
-    string file_path1 = folder1 + new_account + ".txt", file_path2 = folder2 + new_account + ".txt";
+//     string file_path1 = folder1 + new_account + ".txt", file_path2 = folder2 + new_account + ".txt";
 
-    ofstream outfile1(file_path1), outfile2(file_path2);
+//     ofstream outfile1(file_path1), outfile2(file_path2);
 
-    if(fs::exists(file_path1)) {
-        cout << "Tài khoản đã tồn tại" << endl;
-        return 0;
-    }
-    if (!outfile1 || !outfile2) {
-        cerr << "Error" << endl;
-        return 0;
-    }
+//     if(fs::exists(file_path1)) {
+//         cout << "Tài khoản đã tồn tại" << endl;
+//         return 0;
+//     }
+//     if (!outfile1 || !outfile2) {
+//         cerr << "Error" << endl;
+//         return 0;
+//     }
 
-    User person; cin >> person;
-    string new_password = generate_password(person);
-    person.set_account(Account(new_account, new_password));
+//     User person; cin >> person;
+//     string new_password = generate_password(person);
+//     person.set_account(Account(new_account, new_password));
 
-    // thiếu kiểm tra điều kiện nhập vào
-    outfile1 << person;
+//     // thiếu kiểm tra điều kiện nhập vào
+//     outfile1 << person;
 
-    string hash = bcrypt::generateHash(new_password);
-    outfile2 << hash << endl;
+//     string hash = bcrypt::generateHash(new_password);
+//     outfile2 << hash << endl;
 
-    outfile1.close();
-    outfile2.close();
-    return 1;
-}
+//     outfile1.close();
+//     outfile2.close();
+//     return 1;
+// }
 
 
 int main() {
-    cout << "HI" << endl;
+    //thao tác của m ở đây, chỉnh sửa là trong kia
+    //2 file header kia mà thiếu hàm thì m include thư viện vào
     return 0;
 }
