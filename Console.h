222410c095905a8c65e2f9125d448ee581252393
Din <n23dccn146@student.ptithcm.edu.vn>
2024-10-31 15:57:57 +0700

    CHo qanh nguu nhu cac

--- Console.h
+++ Console.h
@@ -1,6 +1,7 @@
 #include<iostream>
 #include "Menu.h"
-
+#include "Account.h"
+#include "gotp.h"
 using namespace std;
 
 namespace Console {
@@ -9,182 +10,23 @@ void print(int x, int y, string s) {
 	cout << s;
 }
 
-string Change(string& title, string& enter_new, string& old_ans, string& old_password) {
-		Menu::Print_OTP();
-		gotoxy(58,3);
-		cout << title;
-		gotoxy(52,9);
-		cout << enter_new;
-		gotoxy(41,6); //password
-				
-		char ch;
-		bool clearedPrompt = false;
-		string password = "";
-				
-		while (true) {
-		    ch = _getch();
-			
-			if(ch == 9) return "";	// tab: back
-			
-		    if (password.length() >= 38 && ch != 8 && ch != 13) {
-		        continue; 
-		    }
-			
-		    if (ch == 13) { // Enter
-		        break;
-		    } else if (ch == 8) { // Backspace
-		        if (!password.empty()) {
-		            password.pop_back();
-		            gotoxy(41 + password.length(), 6);
-		            cout << ' ';
-		            gotoxy(41 + password.length(), 6);
-		        }
-		    } else {
-		        if (!clearedPrompt) {
-		            cout << "               ";
-		            gotoxy(41, 6);
-		            clearedPrompt = true;
-		        }
-		        password.push_back(ch);
-		        cout << "*";
-		    }
-		}
-		
-		gotoxy(41,9); //ans
-		clearedPrompt = false;
-		string ans = "";
-		
-		while (true) {
-		    ch = _getch();
-			
-			if(ch == 9) return "";	// tab: back
-			
-		    if (ans.length() >= 38 && ch != 8 && ch != 13) {
-		        continue; 
-		    }
-		
-		    if (ch == 13) { // Enter
-		    	if(ans.length() > 8) break;
-		    	else continue;
-		    } else if (ch == 8) { // Backspace
-		        if (!ans.empty()) {
-		            ans.pop_back();
-		            gotoxy(41 + ans.length(), 9);
-		            cout << ' ';
-		            gotoxy(41 + ans.length(), 9);
-		        }
-		    } else {
-		        if (!clearedPrompt) {
-		            cout << "                                 ";
-		            gotoxy(41, 9);
-		            clearedPrompt = true;
-		        }
-		        ans.push_back(ch);
-		        cout << ch;
-		    }
-		}
-		
-		gotoxy(56,12); //otp
-		string OTP = "000000"/*               OTPPPPPPPPPPPPPPPPPPPP                  */; 
-		cout << OTP;
-		
-		gotoxy(47,15);
-		char c;
-		string check_otp = "";
-		
-		while(true) {
-			c = _getch();
-			
-			if(c == 9) return ""; //tab
-			
-			else if(check_otp.size() == 6 && c != 8) {
-				if(c == 13) break;
-				continue;
-			}
-			else if(c == 13) continue;
-			else if(c == 8) {
-				if(!check_otp.empty()) {
-					check_otp.pop_back();
-					gotoxy(48 + check_otp.size()*5 - 1,15);
-					cout << " ";
-					gotoxy(48 + check_otp.size()*5 - 1,15);
-				}
-			}
-			else {
-				check_otp.push_back(c);
-				cout << c;
-				if(check_otp.size() == 6)
-					gotoxy(48 + check_otp.size()*5 + 1 -5, 15);
-				else
-					gotoxy(48 + check_otp.size()*5 - 1, 15);
-			}
-		}
-		
-		if(ans == old_ans || password != old_password || OTP != check_otp) {
-			gotoxy(5,25);
-			Sleep(3000);
-			ans = Change(title, enter_new, old_ans, old_password);
-		}
-		
-		Menu::Notification("----Successful operation----",42,5);
-		return ans;
-	}
-
-    
-	bool Create_Account() {
-		Menu::Create_Account_Screen();
-		
-		string create_username = input(41, 7, false, false);
-		if(create_username == "") return 1;
-		
-		string create_password = input(41, 10, false, true);
-		if(create_password == "") return 1;
-		
-		string create_password_again = input(41, 13, false, true);
-		if(create_password_again == "") return 1;
-		
-	
-		if(create_username) { 	// ================================================Kiem tra da ton tai chua=========================)
-			Menu::Notification("Username already exists",50, 5);
-			return false; // da ton tai
-		}
-		if(create_password != create_password_again) {
-			Menu::Notification("Password is incorrect !", 47, 5);
-			return false; // mau khau lan 1 khac mat khau lan 2
-		}
-		
-		//==========================================================Luu thong tin tk va mk===============================================
-		
-		while(true) {
-			if(Create_Information() == 0) {  // Bam tab
-				Create_Account_Screen();
-				print(41, 7, create_username);
-				print(41, 10, create_password);
-				print(41, 13, create_password_again);
-				gotoxy(5, 25);
-				
-				if()
-			}
-		}
-	}
-	
-    string input(int x, int y, bool isPlus, bool ispassword) {
+string input(int x, int y, bool isPlus, bool ispassword) {
     string res = "";
     char ch;
     bool clearedPrompt = false;
 
     gotoxy(x, y);
     while (true) {
-	    ch = _getch();
-			
-		if(ch == 9) {	//tab
-			return "";
-		}
-		if(isPlus && ch == 43) {	// neu dang nhap thi co tao tai khoan moi
-			return "+";
-		}
+        ch = _getch();
+            
+        if(ch == 9) {	//tab
+            return "";
+        }
+        if(isPlus && ch == 43) {	// neu dang nhap thi co tao tai khoan moi
+            return "+";
+        }
         if (res.length() >= 39 && ch != 8 && ch != 13) {
-               continue; 
+            continue; 
         }
 
         if (ch == 13) { // Enter
@@ -206,16 +48,178 @@ string Change(string& title, string& enter_new, string& old_ans, string& old_pas
             }
             res.push_back(ch);
             
-			if(ispassword) {
-				cout << "*";	
-			}
-			else {
-				cout << ch;
-			}
+            if(ispassword) {
+                cout << "*";	
+            }
+            else {
+                cout << ch;
+            }
         }
     }
     return res;
 }
+
+string Change(string& title, string& enter_new, string& old_ans, string& old_password) {
+    Menu::Print_OTP();
+    gotoxy(58,3);
+    cout << title;
+    gotoxy(52,9);
+    cout << enter_new;
+    gotoxy(41,6); //password
+            
+    char ch;
+    bool clearedPrompt = false;
+    string password = "";
+            
+    while (true) {
+        ch = _getch();
+        
+        if(ch == 9) return "";	// tab: back
+        
+        if (password.length() >= 38 && ch != 8 && ch != 13) {
+            continue; 
+        }
+        
+        if (ch == 13) { // Enter
+            break;
+        } else if (ch == 8) { // Backspace
+            if (!password.empty()) {
+                password.pop_back();
+                gotoxy(41 + password.length(), 6);
+                cout << ' ';
+                gotoxy(41 + password.length(), 6);
+            }
+        } else {
+            if (!clearedPrompt) {
+                cout << "               ";
+                gotoxy(41, 6);
+                clearedPrompt = true;
+            }
+            password.push_back(ch);
+            cout << "*";
+        }
+    }
+		
+    gotoxy(41,9); //ans
+    clearedPrompt = false;
+    string ans = "";
+    
+    while (true) {
+        ch = _getch();
+        
+        if(ch == 9) return "";	// tab: back
+        
+        if (ans.length() >= 38 && ch != 8 && ch != 13) {
+            continue; 
+        }
+    
+        if (ch == 13) { // Enter
+            if(ans.length() > 8) break;
+            else continue;
+        } else if (ch == 8) { // Backspace
+            if (!ans.empty()) {
+                ans.pop_back();
+                gotoxy(41 + ans.length(), 9);
+                cout << ' ';
+                gotoxy(41 + ans.length(), 9);
+            }
+        } else {
+            if (!clearedPrompt) {
+                cout << "                                 ";
+                gotoxy(41, 9);
+                clearedPrompt = true;
+            }
+            ans.push_back(ch);
+            cout << ch;
+        }
+    }
+    
+    gotoxy(56,12); //otp
+    string OTP = gotp::generate_otp();
+    cout << OTP;
+    
+    gotoxy(47,15);
+    char c;
+    string check_otp = "";
+    
+    while(true) {
+        c = _getch();
+        
+        if(c == 9) return ""; //tab
+        
+        else if(check_otp.size() == 6 && c != 8) {
+            if(c == 13) break;
+            continue;
+        }
+        else if(c == 13) continue;
+        else if(c == 8) {
+            if(!check_otp.empty()) {
+                check_otp.pop_back();
+                gotoxy(48 + check_otp.size()*5 - 1,15);
+                cout << " ";
+                gotoxy(48 + check_otp.size()*5 - 1,15);
+            }
+        }
+        else {
+            check_otp.push_back(c);
+            cout << c;
+            if(check_otp.size() == 6)
+                gotoxy(48 + check_otp.size()*5 + 1 -5, 15);
+            else
+                gotoxy(48 + check_otp.size()*5 - 1, 15);
+        }
+    }
+    
+    if(ans == old_ans || password != old_password || OTP != check_otp) {
+        gotoxy(5,25);
+        Sleep(3000);
+        ans = Change(title, enter_new, old_ans, old_password);
+    }
+    
+    Menu::Notification("----Successful operation----",42,5);
+    return ans;
+}
+
+    
+bool Create_Account() {
+    Menu::Create_Account_Screen();
+    
+    string create_username = input(41, 7, false, false);
+    if(create_username == "") return 1;
+    
+    string create_password = input(41, 10, false, true);
+    if(create_password == "") return 1;
+    
+    string create_password_again = input(41, 13, false, true);
+    if(create_password_again == "") return 1;
+    
+    /*
+    if(create_username) { 	// ================================================Kiem tra da ton tai chua=========================)
+        Menu::Notification("Username already exists",50, 5);
+        return false; // da ton tai
+    }
+    if(create_password != create_password_again) {
+        Menu::Notification("Password is incorrect !", 47, 5);
+        return false; // mau khau lan 1 khac mat khau lan 2
+    }
+    
+    //==========================================================Luu thong tin tk va mk===============================================
+    */
+
+    // while(true) {
+    // 	if(Create_Information() == 0) {  // Bam tab
+    // 		Menu::Create_Account_Screen();
+    // 		print(41, 7, create_username);
+    // 		print(41, 10, create_password);
+    // 		print(41, 13, create_password_again);
+    // 		gotoxy(5, 25);
+            
+    // 		if()
+    // 	}
+    // }
+}
+	
+    
     
 
     void Change_Information() {
@@ -223,7 +227,7 @@ string Change(string& title, string& enter_new, string& old_ans, string& old_pas
     	bool stop = 1;
     	
     	while (stop) {
-    		Print_Change_Information();
+    		Menu::Print_Change_Information();
 	        gotoxy(4, 27);
 	        cout << "--> Enter your choice: ";
 	
@@ -234,7 +238,7 @@ string Change(string& title, string& enter_new, string& old_ans, string& old_pas
 	    	if(c == 9) return;
 	    	
 	        if (c < '1' || c > '7') {
-	            Notification("----Invalid result----", 42,5);
+	            Menu::Notification("----Invalid result----", 42,5);
 	            Sleep(3000);
 	        }
 		
@@ -270,40 +274,40 @@ string Change(string& title, string& enter_new, string& old_ans, string& old_pas
 	}
 	
 	
-	void Print_Information(){
-	    system("cls");
-	    string str = R"(
-	    IDENTIFICATION INFORMATION
-	    --------------------------------------------------------------------------------------
-	
-	    Full Name:                                 
-	    Date of Birth:
-	    Gender:
-	
-	    ACCOUNT INFORMATION
-	    --------------------------------------------------------------------------------------
-	    Phone Number:
-	    Country:
-		
-        --------------------------------------------------------------------------------------
-                                 Note that pressing the 'Tab' will back	
-	    )";
-	    cout << str;
-	    
-	    
-	    gotoxy(52,4);
-	    string fullname = "a"; //==============================================================
-	    cout << fullname;
-	    gotoxy(52,5);
-	    string age = "a"; //=================================================;
-	    gotoxy(52,6);
-	    string ge;
-	    char ch;
-	    while(1) {
-	    	ch = _getch();
-	    	if(ch != 9)
-	    		continue;
-	    	else break;
-		}
-	}	
+void Print_Information(){
+    system("cls");
+    string str = R"(
+    IDENTIFICATION INFORMATION
+    --------------------------------------------------------------------------------------
+
+    Full Name:                                 
+    Date of Birth:
+    Gender:
+
+    ACCOUNT INFORMATION
+    --------------------------------------------------------------------------------------
+    Phone Number:
+    Country:
+    
+    --------------------------------------------------------------------------------------
+                                Note that pressing the 'Tab' will back	
+    )";
+    cout << str;
+    
+    
+    gotoxy(52,4);
+    string fullname = "a"; //==============================================================
+    cout << fullname;
+    gotoxy(52,5);
+    string age = "a"; //=================================================;
+    gotoxy(52,6);
+    string ge;
+    char ch;
+    while(1) {
+        ch = _getch();
+        if(ch != 9)
+            continue;
+        else break;
+    }
+}	
 }
--- main.cpp
+++ main.cpp
@@ -2,11 +2,12 @@
 #include<fstream>
 #include<filesystem>
 #include <random>
-#include"User.h"
+// #include"User.h"
 #include"gotp.h"
-#include"bcrypt.cpp"
-#include"blowfish.cpp"
+// #include"bcrypt.cpp"
+// #include"blowfish.cpp"
 #include"Console.h"
+#include "Menu.h"
 
 using namespace std;
 namespace fs = filesystem;
@@ -14,14 +15,14 @@ namespace fs = filesystem;
 
 
 // bool create_account() {
-//     auto generate_password = [&](User &person)->string {
-//         string first_name = "", full_name = person.get_full_name();
-//         for(int i = full_name.size() - 1; i >= 0 && full_name[i] != ' '; --i) {
-//             first_name = full_name[i] + first_name;
-//         }
+    // auto generate_password = [&](User &person)->string {
+    //     string first_name = "", full_name = person.get_full_name();
+    //     for(int i = full_name.size() - 1; i >= 0 && full_name[i] != ' '; --i) {
+    //         first_name = full_name[i] + first_name;
+    //     }
 
-//         return first_name + "&" + to_string(person.get_age()) + "&" + to_string(int(person.get_gender()));
-//     };
+    //     return first_name + "&" + to_string(person.get_age()) + "&" + to_string(int(person.get_gender()));
+    // };
 
 //     cout << "Tên tài khoản: ";
 //     string new_account; cin >> new_account;
@@ -59,5 +60,103 @@ namespace fs = filesystem;
 int main() {
     //thao tác của m ở đây, chỉnh sửa là trong kia
     //2 file header kia mà thiếu hàm thì m include thư viện vào
+        bool oke = 1;
+    
+    while(oke) {
+
+    	Menu::printLoginFrame();
+    	
+    	string username = Console::input(41, 7, true, false);
+    	
+    	if(username == "") { // end
+    		break;
+		}
+		if(username == "+") {
+			while(true) {
+				if(Console::Create_Account() == 1) { // bam tab
+					break;
+				}
+			}
+			continue; // tro ve giao dien dang nhap
+		}
+		
+		string password = Console::input(41, 10, true, true);
+		
+		if(password == "") { // end
+    		break;
+		}
+		if(password == "+") {
+			while(true) {
+				if(Console::Create_Account() == 1) { // bam tab
+					break;
+				}
+			}
+			continue; // tro ve giao dien dang nhap
+		}
+			
+		// L?u username và password ==================================================================================================
+		
+		/*if(/* Thong tin khong hop le ----------------------------------------------------------------------------------------------------) {
+			continue; //  Nhap lai
+		}
+		*/
+		
+		
+		bool Manager = 1; // kiem tra neu la quan tri vien ===============================================
+		bool oke1 = 1;
+		
+		/*while(oke1) {
+			char c;
+			
+			while(1) {
+				if(Manager) {
+					Print_Manager_Menu();
+				}
+				else {
+					Print_User_Menu();
+				}
+				
+				gotoxy(4,27);
+				cout << "--> Enter your choice: ";
+				
+				char mx;
+				if(Manager) {	// neu quan tri vien thi 5 phan
+					mx = '5';
+				}
+				else {			// nguoi dung 4 phan
+					mx = '4';
+				}
+				
+				c = _getch(); 
+				cout << c;
+				Sleep(200);
+				
+				if(c == 9) {
+					Exit();
+					return 0;
+				}
+				if (c < '1' || c > mx) {
+	            	Invalid_Result();
+	            	Sleep(3000);
+		        } 
+				else {
+		            break;
+		        }
+			}
+			
+			switch(c) {
+				case '1': {
+					A.Change_Information();
+					break;
+				}
+				case  '2': {
+					A.Print_Information();
+					break;
+				}
+			}	
+		}*/
+	}
+	
+	Menu::Notification("The program has ended", 50, 5); // Thong bao ket thuc
     return 0;
 }
